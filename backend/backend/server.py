a7bfcb3db45fc8a2aa52ff8be1fd4556827ec22f
mannaayd <45467633+mannaayd@users.noreply.github.com>
2021-04-22 16:43:24 +0200

    hello world back

---
+++ backend/backend/server.py
@@ -0,0 +1,19 @@
+import os.path
+
+import flask
+import flask_cors
+
+
+class CollaborativeFilteringApp(flask.Flask):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        flask_cors.CORS(self)
+
+
+app = CollaborativeFilteringApp("collaborative-filtering-app")
+
+env = os.environ.get("APP_ENV", "dev")
+print(f"Starting application in {env} mode")
+app.config.from_object(f"backend.{env}_settings")

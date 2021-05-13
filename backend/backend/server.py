322900092e1c75e90df6ece64607170e2ea30e0a
mannaayd <45467633+mannaayd@users.noreply.github.com>
2021-05-13 09:49:31 +0200

    added pandas logic

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -1,30 +1,62 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect
 from flask_cors import CORS
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+import os
+SECRET_KEY = os.urandom(32)
 
 
 app = Flask(__name__)
-CORS(app)    
+app.config['SECRET_KEY'] = SECRET_KEY
+CORS(app)
+if os.path.exists('method.txt'):
+    os.remove('method.txt')
+if os.path.exists('movies.txt'):
+    os.remove('movies.txt')
 
-film_ratings = []
+class MovieForm(FlaskForm):
+    mv = StringField('Movie', validators=[DataRequired()])
+    rt = StringField('Rating', validators=[DataRequired()])
 
-recomendations = []
+class CorrForm(FlaskForm):
+    mt = StringField('Correlation method', validators=[DataRequired()])
+
+
+
+file_movies = open('movies.txt', 'w')
+movies = []
 
 @app.route("/")
 def index():
-    return "It's home page, here is nothing to see..."
+    return render_template('index.html')
+
+#@app.route("/movies")
+#def movies_():
+#    mvForm = MovieForm()
+#    crForm = CorrForm()
+#    return render_template('movies.html', crForm = crForm, mvForm = mvForm)
 
 # метод запроса фильмов и рейтингов
-@app.route("/recomendations", methods=['POST']) 
-def set_film_ratings():
-    user_ratings = request.json
-    film_ratings.append(user_ratings)
-    return jsonify(film_ratings)
-
-# метод возврата клиенту рекомендаццй
-@app.route("/recomendations", methods=['GET'])
-def get_recomendations():
-    # здесь вызов воркера и загрузка данных в recomendations
-    return jsonify(recomendations)
+@app.route("/movies", methods=['POST', 'GET']) 
+def movie_ratings():
+    movie = None
+    rating = None
+    mvForm = MovieForm()
+    crForm = CorrForm()
+    if mvForm.validate_on_submit():
+        movie = request.form.get('mv')
+        rating = request.form.get('rt')
+        file_movies.write(movie + ' ' + rating + '\n')
+        movies.append([movie, rating])
+    if crForm.validate_on_submit() and len(movies) > 5:
+        file_method = open('method.txt', 'w')
+        file_method.write(request.form.get('mt'))
+        file_method.close()
+        file_movies.close()
+        return redirect('/recomendations')
+    return render_template('movies.html', movies = movies, crForm = crForm, mvForm = mvForm)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.debug = True
+    app.run()
---
+++ backend/worker/__main__.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+import os.path
+import os
+import time
+
+
+
+def creat_user(user_id = None):
+    f = open('movies.txt','r')
+    text = []
+    movie_all = []
+    for line in f:
+        text.append(line)
+
+    df_r = pd.read_csv("storage/ratings.csv")
+    df_m = pd.read_csv('storage/movies.csv')
+    user_id = df_r['userId'].max()+1
+    #print(user_id)
+    for i in text:
+        a, b = i.split()[-1], i[:-3]
+        #print(a,'++',b, '++')
+        movieId = df_m[df_m['title']==b]['movieId'].values[0]
+        movie_all.append(movieId)
+        print(user_id, "  ", movieId, " ", b)
+        df1 = pd.DataFrame({"userId":[user_id],
+                            "movieId":[movieId],
+                            "rating":[float(a)],
+                            "timestamp":[964982703]})
+        df_r = df_r.append(df1,ignore_index = True)
+    #print(df_r)
+    create_corr(user_id,df_r,movie_all)
+
+
+
+
+def create_corr(user_id:int,df,movie_all):
+    print(df)
+    #df = pd.read_csv("ratings.csv",)
+    df = df[['movieId','rating','userId']]
+    dff = pd.pivot_table(df,index=['userId'],columns=['movieId'],values='rating',aggfunc=np.sum)
+    
+    #ddf = dff.fillna(0)
+    #dff = pd.get_dummies(df['user_Id'])
+    a = dff.T.corr(method='spearman')
+    #a.fillna(0)
+    #print(a.sort_values(ascending=False).head(20))
+    dff = dff.fillna(0)
+    a = a.fillna(0)
+    #print(a[user_id].sum())
+    ind = dff.index
+    col = dff.columns
+    #print(dff.loc[1,1],a.loc[1,1])
+    #print(dff.loc[2,1],a.loc[1,2])
+    #print(dff.loc[3,1],a.loc[1,3])
+    #for i in ind:
+    #    for j in col:
+    #        dff.loc[i,j] = dff.loc[i,j]*a.loc[1,i]
+    #    print(i)
+    
+    #print(dff.values)
+    #print(a[1].values)
+    
+    dff = dff.mul(a[user_id].values,axis=0)
+    dff = dff.drop(columns=movie_all)
+    #print(movie_all)
+    #print(dff.sum().sort_values(ascending=False))
+    df_new_res = dff.sum().sort_values(ascending=False)
+    res = []
+    arr_film_res = df_new_res.index[:5]
+    df_m = pd.read_csv("storage/links.csv")
+    for i in arr_film_res:
+        res.append(df_m[df_m['movieId']==i]['tmdbId'].values[0])
+    #print(res)
+    f = open('res.txt','w')
+    for i in res:
+        f.write("https://www.themoviedb.org/movie/"+str(int(i))+"\n")
+    #print(dff.head(10))
+    #print(a.head(10))
+    #df_new_res = 
+    print("result is complete.")
+
+
+
+#create_corr(1)
+
+
+def main():
+    if os.path.exists('res.txt'):
+        os.remove('res.txt')
+    if os.path.exists('method.txt'):
+        os.remove('method.txt')
+    while True:
+        time.sleep(3)
+        check_file = os.path.exists('method.txt')
+        if check_file:
+            creat_user()
+            if os.path.exists('method.txt'):
+                os.remove('method.txt')
+            if os.path.exists('movies.txt'):
+                os.remove('movies.txt')  
+
+
+main()
